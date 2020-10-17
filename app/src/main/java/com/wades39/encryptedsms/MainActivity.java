32ebac6f3e86f41ce034e8a774c8da464cd31f6b
wades39 <kylerswade@gmail.com>
2020-10-16 19:39:18 -0600

    Create Master

---
+++ app/src/main/java/com/wades39/encryptedsms/MainActivity.java
@@ -0,0 +1,14 @@
+package com.wades39.encryptedsms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
